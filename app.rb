6d6a1c838e98651ea8944e61161d7e2e6f71c716
Christine Chee <christinechee88@hotmail.com>
2021-02-19 16:38:31 +0000

    Add view for index and birthday page

--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
 require 'sinatra/base'
+require 'shotgun'
+require './lib/birthday'
 
 class Greeter < Sinatra::Base
+  enable :sessions
 
-get '/' do
-  
-end
+  get '/' do
+    erb :index
+  end
+
+  post '/info' do
+    @name = params[:name]
+    session[:name] = @name
+    $birthday = Birthday.new(params[:day], params[:Month], params[:year])
+    p $birthday
+    if $birthday.is_today?
+      redirect '/birthday'
+    else
+      redirect '/when'
+    end
+  end
+
+  get '/birthday' do
+    @name = session[:name]
+    erb :bday
+  end
+
+  get '/when' do
+    erb :nope
+  end
 
   run! if app_file == $0
 end
