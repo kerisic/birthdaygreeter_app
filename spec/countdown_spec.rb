a2e086975c43d524a5c231f5aff8c49e40173566
Christine Chee <christinechee88@hotmail.com>
2021-02-20 12:54:48 +0000

    remove test

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -5,11 +5,4 @@ describe Countdown do
     subject = Countdown.new("1", "1")
     expect(subject).to be_a Countdown
   end
-
-  describe '#calculate days' do
-    it 'counts how many days are left until the next birthday' do
-      subject = Countdown.new("28", "02")
-      expect(subject.days_left).to eq 9
-    end
-  end
 end
